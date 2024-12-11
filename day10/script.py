e988068ab84c113818f855e908fa66f989d09f38
tade3910 <omotadeogunmodede@gmail.com>
2024-12-10 20:31:41 -0500

    optimized sol2

--- day10/script.py
+++ day10/script.py
@@ -10,25 +10,22 @@ def findTrailHeads(input:list[list[str]]):
 def isInWorld(row,col,world:list[list]):
     return row >= 0 and row < len(world) and col >= 0 and col < len(world[row])
 
-def find9s(row:int,col:int,input:list[list[str]],found:set[str],paths:set[str],prev:int=-1,path=""):
+def find9s(row:int,col:int,input:list[list[str]],found:set[str],prev:int=-1):
     if not isInWorld(row,col,input):
-        return 
+        return 0
     curVal = input[row][col]
     if curVal == '.':
-        return 
-    locString = f"{row}:{col}"
-    path = path + locString
+        return 0
     curVal = int(curVal)
     if prev + 1 != curVal:
-        return 
+        return 0
     if curVal == 9:
-        found.add(locString)
-        paths.add(path)
-        return
-    find9s(row,col -1,input,found,paths,curVal,path)
-    find9s(row,col + 1,input,found,paths,curVal,path)
-    find9s(row - 1,col,input,found,paths,curVal,path)
-    find9s(row + 1,col,input,found,paths,curVal,path)
+        found.add(f"{row}:{col}")
+        return 1
+    return (find9s(row,col -1,input,found,curVal) + 
+            find9s(row,col + 1,input,found,curVal) +
+            find9s(row - 1,col,input,found,curVal) + 
+            find9s(row + 1,col,input,found,curVal))
    
 
 def parseInput(fileName:str):
@@ -42,10 +39,8 @@ def parseInput(fileName:str):
         for trailHead in trailHeads:
             row,col = trailHead
             found = set()
-            paths = set()
-            find9s(row,col,input,found,paths)
+            part2Count = part2Count + find9s(row,col,input,found)
             part1Count = part1Count + len(found)
-            part2Count = part2Count + len(paths)
         return part1Count,part2Count
 
 def main():
