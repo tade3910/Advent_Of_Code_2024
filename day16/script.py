7153f0dd63df87fe72f44fe8833598be6535ede8
tade3910 <omotadeogunmodede@gmail.com>
2024-12-16 13:01:42 -0500

    completed part2

--- day16/script.py
+++ day16/script.py
@@ -1,6 +1,5 @@
 
 import heapq
-
 WALL = '#'
 START = 'S'
 END = 'E'
@@ -15,50 +14,80 @@ def getOpositeDirection(direction:str):
      else:
           return 'N'
 
-def exploreNeigbors(world:list[list[str]],currentEntry:tuple[int,int,str],currentDistance,queu:list[tuple[int,int,str]],distances:dict[tuple[int,int],int],prevs:dict[tuple[int,int],tuple[int,int]]):
+def getNeigbor(currentDirection:str,displacement:tuple[int,int,str],curentRow:int,currentCol:int):
+     neigborDirection = displacement[2] 
+     if neigborDirection == currentDirection:
+          return curentRow + displacement[0],currentCol + displacement[1],1
+     elif neigborDirection == getOpositeDirection(currentDirection):
+          return curentRow,currentCol,2000
+     else:
+          return curentRow,currentCol,1000
+
+def exploreNeigbors(world:list[list[str]],currentEntry:tuple[int,int,str],currentDistance,queu:list[tuple[int,int,str]],distances:dict[tuple[int,int,str],int],prevs:dict[tuple[int,int,str],list[tuple[int,int,str]]]):
         curentRow,currentCol,currentDirection = currentEntry
         displacements = [(-1,0,'N'),(1,0,'S'),(0,-1,'W'),(0,1,'E')]
-        opositeDirection = getOpositeDirection(currentDirection)
         for displacement in displacements:
-             neigborRow = curentRow + displacement[0]
-             neigborCol = currentCol + displacement[1]
+             neigborRow,neigborCol,scoreChange = getNeigbor(currentDirection,displacement,curentRow,currentCol)
              if world[neigborRow][neigborCol] == WALL: 
                   continue
              neigborDirection = displacement[2]
-             scoreChange = 1001
-             if currentDirection == neigborDirection: scoreChange = 1
-             elif currentDirection == opositeDirection: scoreChange = 2001
              newDistance = currentDistance + scoreChange
-             oldDistance = distances.get((neigborRow,neigborCol),float('inf'))
-             if newDistance < oldDistance:
-                  neigbor = neigborRow,neigborCol
-                  neigborEntry = neigborRow,neigborCol,neigborDirection
-                  distances[neigbor] = newDistance
-                  prevs[neigbor] = (curentRow,currentCol)
-                  heapq.heappush(queu,(newDistance,neigborEntry))
-    
-
+             neigborEntry = neigborRow,neigborCol,neigborDirection
+             oldDistance = distances.get(neigborEntry,float('inf'))
+             if newDistance <= oldDistance:
+                  if newDistance < oldDistance:
+                       prevs[neigborEntry] = []
+                       distances[neigborEntry] = newDistance
+                  prevs[neigborEntry].append(currentEntry)
+                  heapq.heappush(queu,(newDistance,neigborEntry))    
+             
 def dikstras(world:list[list[str]],start:tuple[int,int]):
     queu = []
     startRow,startCol = start
-    distances = {start:0}
+    startEntry = startRow,startCol,'E'
+    distances = {startEntry:0}
     visited = set()
-    prevs = {}
-    heapq.heappush(queu,(0,(startRow,startCol,'E')))
+    prevs = {startEntry:None}
+    heapq.heappush(queu,(0,startEntry))
     while queu:
         currentDistance,currentEntry = heapq.heappop(queu)
-        curentRow,currentCol,_ = currentEntry
-        curentLocation = curentRow,currentCol
-        if curentLocation in visited: continue
-        visited.add(curentLocation)
+        if currentEntry in visited: continue
+        visited.add(currentEntry)
         exploreNeigbors(world,currentEntry,currentDistance,queu,distances,prevs)
     return distances,prevs
 
-def debug(prevs:dict[tuple[int,int],tuple[int,int]],endLocation:tuple[int,int],startLocation:tuple[int,int],distances:dict[tuple[int,int]]):
-     curLocation = endLocation
-     while curLocation != startLocation:
-          print(f"curLocation is {curLocation} and distance is {distances[curLocation]}")
-          curLocation = prevs[curLocation]
+def getPossibleEnds(endEntry:tuple[int,int]):
+    possibilities = []
+    endRow,endCol = endEntry
+    for direction in ['N','S','E','W']:
+         possibilities.append((endRow,endCol,direction))
+    return possibilities
+     
+
+#row 7 col 5
+def countTiles(prevs:dict[tuple[int,int,str],list[tuple[int,int,str]]],endEntry:tuple[int,int,str],startEntry:tuple[int,int,str]):
+    startLocation = startEntry[0],startEntry[1]
+    endLocation = endEntry[0],endEntry[1]
+    tiles = set([startLocation,endLocation])
+    queu = [endEntry]
+    while queu:
+    #   print(f"curLocation is {curLocation} and distance is {distances[curLocation]}")
+        curLocation = queu.pop()
+        if curLocation == startEntry:
+             continue
+        for prev in prevs[curLocation]:
+             tile = (prev[0],prev[1])
+             tiles.add(tile)
+             queu.append(prev)
+    return len(tiles)
+
+def getShortest(distances:dict[tuple[int, int, str], int],endLocation:tuple[int,int]):
+    possibleEnds = getPossibleEnds(endLocation)
+    shortestEnd = 0
+    for i in range(len(possibleEnds)):
+        if distances[possibleEnds[i]] < distances[possibleEnds[shortestEnd]]:
+            shortestEnd = i
+    return distances[possibleEnds[shortestEnd]],possibleEnds[shortestEnd]
 
 def parseInput(fileName:str):
     with open(fileName, 'r') as file:
@@ -74,9 +103,12 @@ def parseInput(fileName:str):
                 elif char == END:
                      endLocation = (i,j)
             world.append(row)
+        
         distances,prevs = dikstras(world,startLocation)
-        # debug(prevs,endLocation,startLocation,distances)
-        print(distances[endLocation])
+        distance,endEntry = getShortest(distances,endLocation)
+        startEntry = (startLocation[0],startLocation[1],'E')
+        count = countTiles(prevs,endEntry,startEntry)
+        print(f"distance is {distance} and count is {count}")
 
 def main():
     parseInput('test.txt')
