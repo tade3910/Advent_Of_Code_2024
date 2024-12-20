7048821e25934c5c0b4962d18b11f91cf2935c73
tade3910 <omotadeogunmodede@gmail.com>
2024-12-20 11:09:48 -0500

    Manhattan circle for the save

---
+++ day20/script2.py
@@ -0,0 +1,76 @@
+START = "S"
+END = "E"
+WALL = "#"
+
+def inWorld(row:int,col:int,track:list[list[str]]):
+    return row >= 0 and col >= 0 and row < len(track) and col < len(track[row])
+
+def dfs(track:list[list[str]],start:tuple[int,int],end:tuple[int,int]):
+    path:list[str] = []
+    visited:set[tuple[int,int]] = set()
+    queue = [start]
+    while queue:
+        curr = queue.pop()
+        visited.add(curr)
+        row,col = curr
+        path.append(curr)
+        if curr == end:
+            break
+        for displacement in [(0,-1),(0,1),(-1,0),(1,0)]:
+            neigborRow, neigborCol = row + displacement[0], col + displacement[1]
+            neigbor = neigborRow,neigborCol
+            if neigbor in visited:
+                continue
+            if not inWorld(neigborRow,neigborCol,track):
+                continue
+            if track[neigborRow][neigborCol] == WALL:
+                continue
+            queue.append(neigbor)
+    return path
+
+def getCheats(path:list[str],maxCheats:int,threshold:int):
+    raceTime = len(path) - 1
+    count = 0
+    for i in range(len(path)-1): # try going from one point in the path to another
+        for j in range(i+1, len(path)):
+            posA, posB = path[i], path[j]
+            distance = abs(posA[0] - posB[0]) + abs(posA[1] - posB[1])
+            if distance > maxCheats: # can't go that far
+                continue
+            cheatTime = (i + 1) + distance + (raceTime - j - 1)
+            timeSaved = raceTime - cheatTime
+            if timeSaved < threshold:
+                continue
+            count += 1
+    print(count)
+
+def getTrack(fileName:str):
+    with open(fileName, 'r') as file:
+        track = []
+        start = 0,0
+        end = 0,0
+        for i,line in enumerate(file):
+            curLine = line.strip()
+            curRow = []
+            for j,col in enumerate(curLine):
+                curRow.append(col)
+                if col == END:
+                    end = i,j
+                elif col == START:
+                    start = i,j
+            track.append(curRow)
+        return track,start,end
+
+def getBestCheatCounts(fileName:str,maxCheats:int,threshold:int):
+    track,start,end = getTrack(fileName)
+    path = dfs(track,start,end)
+    getCheats(path,maxCheats,threshold)
+
+def main():
+    getBestCheatCounts("test.txt",2,1)
+    getBestCheatCounts("input.txt",2,100)
+    getBestCheatCounts("test.txt",20,50)
+    getBestCheatCounts("input.txt",20,100)
+
+if __name__ == "__main__":
+    main()
