220c587387431228593b2c56dfcec28bf484cdf3
tade3910 <omotadeogunmodede@gmail.com>
2024-12-22 14:26:44 -0500

    completed part2

--- day22/script.py
+++ day22/script.py
@@ -37,12 +37,49 @@ def generateXSecretNumbers(secretNumber:int,x:int):
         secretNumber = generateNextSecret(secretNumber)
     return secretNumber
 
-def simulate(fileName:int,x:int):
+def simulateBuys(secretNumber:int,x:int,sums:dict[tuple[int,int,int,int],int]):
+    inital = [0,0,0,0]
+    i = 0
+    while i < 4:
+        prevVal = secretNumber % 10
+        secretNumber = generateNextSecret(secretNumber)
+        curVal = secretNumber % 10
+        change = curVal - prevVal
+        inital[i] = change
+        i += 1
+    sequence = inital[0], inital[1], inital[2], inital[3]
+    assigned = set([sequence])
+    sums[sequence] = sums.get(sequence,0) + curVal
+    for i in range(x):
+        prevVal = secretNumber % 10
+        secretNumber = generateNextSecret(secretNumber)
+        curVal = secretNumber % 10
+        change = curVal - prevVal
+        sequence = sequence[1], sequence[2], sequence[3], change
+        if sequence not in assigned:
+            assigned.add(sequence)
+            sums[sequence] = sums.get(sequence,0) + curVal
+
+def partTwo(fileName:int,x:int):
+    secretNumbers = parseInput(fileName)
+    sums = {}
+    for secretNumber in secretNumbers:
+        simulateBuys(secretNumber,x,sums)
+    max = 0
+    for val in sums.values():
+        if val > max:
+            max = val
+    print(max)
+
+
+def partOne(fileName:int,x:int):
     secretNumbers = parseInput(fileName)
     sum = 0
     for secretNumber in secretNumbers:
         sum += generateXSecretNumbers(secretNumber,x)
     print(f"sum is {sum}")
 
-simulate("test.txt",2000)
-simulate("input.txt",2000)
+partOne("test.txt",2000)
+partOne("input.txt",2000)
+partTwo("test3.txt",2000)
+partTwo("input.txt",2000)
