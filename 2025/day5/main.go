86af1688d33d4c2b76c985d7f92ae5904d4227b9
tade3910 <omotadeogunmodede@gmail.com>
2025-12-05 04:19:54 -0500

    sleepy weepy

---
+++ 2025/day5/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type idRange struct {
+	Max int
+	Min int
+}
+
+func inRange(start, end int, curRuange *idRange) bool {
+	return start >= curRuange.Min && end <= curRuange.Min
+}
+
+func getInt(v string) int {
+	if v == "" {
+		return 0
+	}
+	val, err := strconv.Atoi(v)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return val
+}
+
+func part1(file_name string) {
+	file, err := os.Open(file_name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	validRanges := []*idRange{}
+	isRange := true
+	numValids := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			isRange = false
+			continue
+		}
+		line = strings.TrimSpace(line)
+		if isRange {
+			splitLine := strings.Split(line, "-")
+			start, end := getInt(splitLine[0]), getInt(splitLine[1])
+			validRanges = append(validRanges, &idRange{
+				Max: end,
+				Min: start,
+			})
+		} else {
+			id := getInt(line)
+			for _, validRange := range validRanges {
+				if id <= validRange.Max && id >= validRange.Min {
+					numValids++
+					break
+				}
+			}
+		}
+	}
+	fmt.Printf("There are: %d valid ids\n", numValids)
+}
+
+func getValidRanges(file_name string) []*idRange {
+	file, err := os.Open(file_name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	validRanges := []*idRange{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			return validRanges
+		}
+		line = strings.TrimSpace(line)
+		splitLine := strings.Split(line, "-")
+		start, end := getInt(splitLine[0]), getInt(splitLine[1])
+		validRanges = append(validRanges, &idRange{
+			Max: end,
+			Min: start,
+		})
+	}
+	return validRanges
+}
+
+func shrinkRanges(validRanges []*idRange) []*idRange {
+	shrunk_ranges := []*idRange{}
+	for _, curRuange := range validRanges {
+		start, end := curRuange.Min, curRuange.Max
+		for _, validRange := range validRanges {
+			if curRuange == validRange {
+				//skip same pointer
+				continue
+			}
+			if start < validRange.Min && end >= validRange.Min {
+				//Extend left
+				validRange.Min = start
+			}
+			if end > validRange.Max && start <= validRange.Max {
+				//Extend Right
+				validRange.Max = end
+			}
+		}
+	}
+	//We write over ranges all the time, use map to remove duplicates
+	range_map := make(map[idRange]bool)
+	for _, validRange := range validRanges {
+		_, ok := range_map[*validRange]
+		if !ok {
+			range_map[*validRange] = true
+			shrunk_ranges = append(shrunk_ranges, validRange)
+		}
+	}
+	return shrunk_ranges
+}
+
+func part2(file_name string) {
+	numValids := 0
+	for _, curRuange := range shrinkRanges(getValidRanges(file_name)) {
+		numValids += (curRuange.Max - curRuange.Min + 1)
+	}
+	fmt.Printf("There are: %d valid ids\n", numValids)
+}
+
+func main() {
+	part1("sample.txt")
+	part1("input.txt")
+	part2("sample.txt")
+	part2("input.txt")
+}
+
+/**
+3-5
+10-14 -> 3 < 10 but 5 < 10,
+16-20
+12-18
+
+3 -5
+
+*/
